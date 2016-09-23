35616ae9021710ea4eb56aecf40739e6144768da
D0gi <s122106@hftl.de>
2016-09-23 16:52:28 +0000

    cpui2c.c now gets time and display it on ledmatrix

--- cpui2c/cpui2c.sh
+++ cpui2c/cpui2c.sh
@@ -8,21 +8,30 @@
 # After that, it will use that config file whenever that user runs top,
 # and keep the same appearance, even in batch mode.
 
+fill=2046
 
 
 
+while true;
+ do
+	time=$(date +%H%M%S)
+	for ((j=0, r=0x16; j<6; j++, r-=2));
+	 do
+		row1=$(printf '0x%02x\n' $r)
+		r=$(($r-2))
+		row2=$(printf '0x%02x\n' $r)
+		n=${time:$j:1}
 
+		#for ((i=0, pow=1, x=0; i<n; ++i)); do ((x+=((pow *= 2)))); done
+		x=$(( $fill >> 11-$n ))
+		
+		l=$(printf '0x%02x\n' $(($x & 0x00ff)) )
+		h=$(printf '0x%02x\n' $(($x >> 8)))
 
-m=10
-
-for ((i=1, pow=1, x=1; i<=m; i++)); do ((x+=((pow *= 2)))); done
-
-# echo $pow
-# echo $x
-# printf '0x%04x\n' $x
-# printf '0x%04x\n' $(($x >> 8))
-
-l=$(printf '0x%02x\n' $(($x & 0x00ff)) )
-h=$(printf '0x%02x\n' $(($x >> 8)))
-
-echo $h $l
+		echo i2cset -y 1 0x03 $row1 $l $h i
+		echo i2cset -y 1 0x03 $row2 $l $h i
+		echo
+		i2cset -y 1 0x03 $row1 $l $h i
+		i2cset -y 1 0x03 $row2 $l $h i
+	 done
+ done
